a8bf7481ec816378a90b50780411c95af84a0320
Jacqueline Hettel <hettelj@stanford.edu>
2013-03-28 11:26:01 -0700

    Reconfigured semantic field mappings to reflect structure of data for email generation.

--- app/models/solr_document.rb
+++ app/models/solr_document.rb
@@ -4,19 +4,10 @@ class SolrDocument
   include Blacklight::Solr::Document
 
   # self.unique_key = 'id'
-  
-  # The following shows how to setup this blacklight document to display marc documents
-  extension_parameters[:marc_source_field] = :marc_display
-  extension_parameters[:marc_format_type] = :marcxml
-  use_extension( Blacklight::Solr::Document::Marc) do |document|
-    document.key?( :marc_display  )
-  end
+
   
   # Email uses the semantic field mappings below to generate the body of an email.
   SolrDocument.use_extension( Blacklight::Solr::Document::Email )
-  
-  # SMS uses the semantic field mappings below to generate the body of an SMS email.
-  SolrDocument.use_extension( Blacklight::Solr::Document::Sms )
 
   # DublinCore uses the semantic field mappings below to assemble an OAI-compliant Dublin Core document
   # Semantic mappings of solr stored fields. Fields may be multi or
@@ -25,9 +16,10 @@ class SolrDocument
   # Recommendation: Use field names from Dublin Core
   use_extension( Blacklight::Solr::Document::DublinCore)    
   field_semantics.merge!(    
-                         :title => "title_display",
-                         :author => "author_display",
-                         :language => "language_facet",
-                         :format => "format"
+                         :title => "title",
+                         :composer => "composer",
+                         :librettist => "librettist",
+                         :year => "year"
+                         :genre => "genre"
                          )
 end
