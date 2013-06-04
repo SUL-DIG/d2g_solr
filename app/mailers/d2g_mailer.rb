a60f2655c60031d2a53d24b7ee4e1605b7600eed
Jacqueline Hettel <hettelj@stanford.edu>
2013-06-04 11:21:21 -0700

    Updated mailer.

--- app/mailers/d2g_mailer.rb
+++ app/mailers/d2g_mailer.rb
@@ -1,5 +1,5 @@
 class FrdaMailer < ActionMailer::Base
-  default from: "no-reply@frda.stanford.edu"
+  default from: "no-reply@openingnight.stanford.edu"
 
   def contact_message(opts={})
     params=opts[:params]
@@ -9,15 +9,15 @@ class FrdaMailer < ActionMailer::Base
     @name=params[:name]
     @subject=params[:subject]
     @from=params[:from]
-    to=Frda::Application.config.contact_us_recipients[@subject]
-    cc=Frda::Application.config.contact_us_cc_recipients[@subject]    
-    mail(:to=>to,:cc=>cc, :subject=>"Contact Message from the French Revolution Digital Archive - #{@subject}") 
+    to=D2g::Application.config.contact_us_recipients[@subject]
+    cc=D2g::Application.config.contact_us_cc_recipients[@subject]    
+    mail(:to=>to,:cc=>cc, :subject=>"Contact Message from the Opening Night! Opera and Oratorio Premieres - #{@subject}") 
   end
 
   def error_notification(opts={})
     @exception=opts[:exception]
     @mode=Rails.env
-    mail(:to=>Frda::Application.config.exception_recipients, :subject=>"FRDA Exception Notification running in #{@mode} mode")
+    mail(:to=>D2g::Application.config.exception_recipients, :subject=>"D2g Exception Notification running in #{@mode} mode")
   end
   
 end
