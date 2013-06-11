a1be38ca27ca7e8dde78a51260cd7a419bf22af1
Jacqueline Hettel <hettelj@stanford.edu>
2013-06-11 10:08:14 -0700

    Updating mailer.

--- app/mailers/d2g_mailer.rb
+++ app/mailers/d2g_mailer.rb
@@ -31,23 +31,23 @@ class D2gMailer < ActionMailer::Base
       end
     end
   
-  def contact_message(opts={})
-    params=opts[:params]
-    @request=opts[:request]
-    @message=params[:message]
-    @email=params[:email]
-    @name=params[:name]
-    @subject=params[:subject]
-    @from=params[:from]
-    to=D2g::Application.config.contact_us_recipients[@subject]
-    cc=D2g::Application.config.contact_us_cc_recipients[@subject]    
-    mail(:to=>to,:cc=>cc, :subject=>"Contact Message from the Opening Night! Opera and Oratorio Premieres - #{@subject}") 
-  end
-
-  def error_notification(opts={})
-    @exception=opts[:exception]
-    @mode=Rails.env
-    mail(:to=>D2g::Application.config.exception_recipients, :subject=>"Opening Night! Exception Notification running in #{@mode} mode")
-  end
+#   def contact_message(opts={})
+#     params=opts[:params]
+#     @request=opts[:request]
+#     @message=params[:message]
+#     @email=params[:email]
+#     @name=params[:name]
+#     @subject=params[:subject]
+#     @from=params[:from]
+#     to=D2g::Application.config.contact_us_recipients[@subject]
+#     cc=D2g::Application.config.contact_us_cc_recipients[@subject]    
+#     mail(:to=>to,:cc=>cc, :subject=>"Contact Message from the Opening Night! Opera and Oratorio Premieres - #{@subject}") 
+#   end
+# 
+#   def error_notification(opts={})
+#     @exception=opts[:exception]
+#     @mode=Rails.env
+#     mail(:to=>D2g::Application.config.exception_recipients, :subject=>"Opening Night! Exception Notification running in #{@mode} mode")
+#   end
   
 end
