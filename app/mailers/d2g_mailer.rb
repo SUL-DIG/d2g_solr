a640df6abcd84d40bac8d41e4ac63259714969a1
Jacqueline Hettel <hettelj@stanford.edu>
2013-06-11 10:07:01 -0700

    Updating mailer.

--- app/mailers/d2g_mailer.rb
+++ app/mailers/d2g_mailer.rb
@@ -1,6 +1,36 @@
 class D2gMailer < ActionMailer::Base
   default from: "hettelj@stanford.edu"
 
+  def email
+      @response, @documents = get_solr_response_for_field_values(SolrDocument.unique_key,params[:id])
+      if request.post?
+        if params[:to]
+          url_gen_params = {:host => request.host_with_port, :protocol => request.protocol}
+          
+          if params[:to].match(/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)
+            email = RecordMailer.email_record(@documents, {:to => params[:to], :message => params[:message]}, url_gen_params)
+          else
+            flash[:error] = I18n.t('blacklight.email.errors.to.invalid', :to => params[:to])
+          end
+        else
+          flash[:error] = I18n.t('blacklight.email.errors.to.blank')
+        end
+
+        unless flash[:error]
+          email.deliver 
+          flash[:success] = "Email sent"
+          redirect_to catalog_path(params['id']) unless request.xhr?
+        end
+      end
+
+      unless !request.xhr? && flash[:success]
+        respond_to do |format|
+          format.js { render :layout => false }
+          format.html
+        end
+      end
+    end
+  
   def contact_message(opts={})
     params=opts[:params]
     @request=opts[:request]
