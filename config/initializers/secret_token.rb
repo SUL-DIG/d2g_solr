aa0907ddb836e341d39886f0b248174d8414e7b9
hettelj <hettelj@stanford.edu>
2013-08-20 14:09:00 -0700

    Delete secret_token.rb

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-D2g::Application.config.secret_token = '134751c0d55737a726188bea7d48927c497681343bb14aa3abc3db422448d057c8606cd3be8bb0a825d494536e8e153e2abfca16c565cff6792392e3f8a1704c'
