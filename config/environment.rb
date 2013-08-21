99280d5748803e67b238f4125f37d750632c24b2
Jacqueline Hettel <hettelj@stanford.edu>
2013-08-21 11:48:12 -0700

    Configuring record mailer.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 D2g::Application.initialize!
+
+ActionMailer::Base.default(:from => "no-reply@stanford.edu")
