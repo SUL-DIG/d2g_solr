d08ae6b4d13116e2dc6c5479a0e4c8301c6161e6
Jacqueline Hettel <hettelj@stanford.edu>
2013-08-21 10:15:52 -0700

    Configured for sending email on server.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,10 @@ D2g::Application.configure do
   
   #Configuring email for sending from server.
   config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.default_options = {:from => "opening_night@lists.stanford.edu"}
+  config.action_mailer.perform_deliveries = true
+config.action_mailer.raise_delivery_errors = true
+config.action_mailer.default_options = {from: 'no-reply@stanford.edu'}
+
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
